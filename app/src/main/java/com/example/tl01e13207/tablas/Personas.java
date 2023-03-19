364fde513ecf3607c807ab2561093b5a54dba4db
JosuéH <jcch221@gmail.com>
2023-03-19 11:15:47 -0600

    Initial commit

--- app/src/main/java/com/example/tl01e13207/tablas/Personas.java
+++ app/src/main/java/com/example/tl01e13207/tablas/Personas.java
@@ -1,4 +1,41 @@
 package com.example.tl01e13207.tablas;
 
 public class Personas {
+
+    private int id;
+    private String nombre;
+    private String numero;
+    private String nota;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getNumero() {
+        return numero;
+    }
+
+    public void setNumero(String numero) {
+        this.numero = numero;
+    }
+
+    public String getNota() {
+        return nota;
+    }
+
+    public void setNota(String nota) {
+        this.nota = nota;
+    }
 }
